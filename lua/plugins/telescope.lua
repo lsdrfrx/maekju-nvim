057db7b3373d251ba92e2c4d001a9b5490f890e7
Christian Guetnga <lsdrfrx@gmail.com>
2023-11-28 23:57:59 +0300

    refactored tab width

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,14 @@
 require("telescope").setup({
-    pickers = {
-        buffers = {
-            initial_mode = "normal",
-            show_all_buffers = true,
-            sort_mru = true,
-            mappings = {
-                n = {
-                    ["<c-d>"] = "delete_buffer",
-                }
-            }
-        }
-    }
+	pickers = {
+		buffers = {
+			initial_mode = "normal",
+			show_all_buffers = true,
+			sort_mru = true,
+			mappings = {
+				n = {
+					["<c-d>"] = "delete_buffer",
+				},
+			},
+		},
+	},
 })
