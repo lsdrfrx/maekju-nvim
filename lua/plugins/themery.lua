c5097928c8a9c4bc355484ce9977d42b94c392ed
Christian Guetnga <lsdrfrx@gmail.com>
2023-11-28 23:50:18 +0300

    updated nvim theme

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -1,16 +1,18 @@
-require('themery').setup({
-    themes = {
-        'tokyonight-storm',
-        'tokyonight-night',
-        'tokyonight-moon',
-        'tokyonight-day',
-    },
-    themeConfigFile = "~/.config/nvim/plugins/themery.lua",
-    livepreview = true,
+require("themery").setup({
+	themes = {
+		"tokyonight-storm",
+		"tokyonight-night",
+		"tokyonight-moon",
+		"tokyonight-day",
+		"everforest",
+		"catppuccin-frappe",
+	},
+	themeConfigFile = "~/.config/nvim/lua/plugins/themery.lua",
+	livepreview = true,
 })
 
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme tokyonight-storm")
-vim.g.theme_id = 1
+vim.cmd("colorscheme catppuccin-frappe")
+vim.g.theme_id = 6
 -- end themery block
