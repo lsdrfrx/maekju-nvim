be913148cb957c050af566897126ecf0fcde2de3
Christian Guetnga <lsdrfrx@gmail.com>
2024-05-15 13:11:25 +0300

    Small refactoring

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,10 +1,6 @@
 local map = vim.keymap.set
 
 map("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
-map("n", "<leader>q", ":CodeActionMenu<CR>")
-map("n", "K", function()
-	vim.diagnostic.open_float()
-end)
 map("n", "<C-BS>", ":w<CR>:bd<CR>", { silent = true })
 map("n", "<ESC>", ":noh<CR><ESC>", { silent = true })
 map("i", "<C-BS>", "<C-w>", { silent = true })
@@ -21,15 +17,16 @@ map("n", "<A-j>", ":wincmd j<CR>", { silent = true })
 map("n", "<A-k>", ":wincmd k<CR>", { silent = true })
 map("n", "<A-l>", ":wincmd l<CR>", { silent = true })
 
+--
 -- Git keymaps
+--
 map("n", "<leader>g", ":Git<CR>", { silent = true })
 map("n", "<leader>gd", ":Gvdiffsplit<CR>", { silent = true })
 map("n", "<leader>gp", ":Git push<CR>", { silent = true })
 
--- Mind keymaps
-map("n", "<leader>M", ":MindOpenProject<CR>", { silent = true })
-
+--
 -- Harpoon keymaps
+--
 map("n", "<leader>h", function()
 	require("harpoon.ui").toggle_quick_menu()
 end)
@@ -37,20 +34,26 @@ map("n", "<leader>m", function()
 	require("harpoon.mark").add_file()
 end)
 
+--
 -- Code actions keymaps
+--
 map("i", "<F2>", function()
 	require("renamer").rename()
 end, { noremap = true, silent = true })
+
 map("n", "<F2>", function()
 	require("renamer").rename()
 end, { noremap = true, silent = true })
 
+-- Run current buffer
 map("n", "<F5>", function()
   local extension = vim.bo.filetype
-
   if extension == "python" then
     vim.cmd("!python " .. vim.fn.expand("%"))
   end
-
-
 end, { noremap = true })
+
+-- Open float diagnostics
+map("n", "K", function()
+	vim.diagnostic.open_float()
+end)
