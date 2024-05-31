81a047196a8068c303114ec77fa1393e1ac8cda7
Christian Guetnga <lsdrfrx@gmail.com>
2024-05-31 19:45:43 +0300

    Moved neovide configuration to separate file, removed unused buttons from alpha

--- init.lua
+++ init.lua
@@ -1,10 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
-if vim.g.neovide then
-  vim.g.neovide_fullscreen = true
-  vim.o.guifont = "DejaVuSansM Nerd Font:h13"
-end
+require("neovide-init")
 
 require("plugin-setup")
 
---
+++ lua/neovide-init.lua
@@ -0,0 +1,7 @@
+if vim.g.neovide then
+	vim.o.guifont = "DejaVuSansM Nerd Font:h15"
+	vim.g.neovide_padding_top = 20
+	vim.g.neovide_underline_stroke_scale = 2.0
+	vim.g.neovide_padding_bottom = 20
+	vim.g.neovide_fullscreen = true
+end
